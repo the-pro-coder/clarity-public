ecb693dc8a5eb774db2366392d3f75fbc707801e
the-pro-coder <hruggeri14@gmail.com>
2025-11-28 20:09:44 -0600

    minor fix

--- clarity-app/app/page.tsx
+++ clarity-app/app/page.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react/jsx-runtime";
 import LandingPageHeader from "@/components/custom/landing page/LandingPageHeader";
-import LandingPageHeroSection from "@/components/custom/landing page/LandingPageHeroSection";
 export default function Home() {
   return (
     <Fragment>
